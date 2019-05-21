25cb3699ddb56b93b00ac8465ea6f55c68adb54d
ihybrid18542324 <50878460+ihybrid18542324@users.noreply.github.com>
2019-05-21 17:08:47 +0100

    Add files via upload

---
+++ Satisfying_library_2019_05_21_16_06_42/sketch.js
@@ -0,0 +1,81 @@
+var img;
+
+var columns = 30;
+var rows = 30;
+
+// This is an array that will contain all our circles
+var circles = [];
+
+var colSize;
+var rowSize;
+
+// Preloads our image
+function preload() {
+  img = loadImage("test.jpg");
+}
+
+function setup() {
+  createCanvas(400, 400);
+  
+  // Here we load the pixels in the image and work out
+  // what colour each circle will be
+  img.loadPixels();
+  
+  colSize = width / columns;
+  rowSize = height / rows;
+  
+  for (var x = 0; x < columns; x++) {
+    for (var y = 0; y < rows; y++) {
+      var c = img.get(x * colSize, y * rowSize);
+      
+      // We create a new circle and add it to our circles array
+      circles.push(new Circle(x * colSize, y * rowSize, colSize, rowSize, c));
+    }
+  }
+  
+  noStroke();
+}
+
+function draw() {
+  background(220);
+  
+  // We iterate over our circles and draw each one
+  for (let i = 0; i < circles.length; i++) {
+    circles[i].draw(); 
+  }
+}
+
+function mouseDragged() {
+  var currX = floor(mouseX / colSize);
+  var currY = floor(mouseY / rowSize);
+  
+  var index = (currX * rows) + currY;
+  
+  circles[index].grow();
+}
+
+
+// This is our circle class, each circle is a type of this
+class Circle {
+  constructor(x, y, w, h, c) {
+    this.x = x;
+    this.y = y;
+    
+    this.w = w;
+    this.h = h;
+    
+    this.c = c;
+    
+    this.scale = 0.1;
+  }
+  
+  draw() {
+    fill(this.c);
+    ellipse(this.x, this.y, this.w * this.scale, this.h * this.scale);
+  }
+  
+  grow() {
+    this.scale += 0.2;
+    this.scale = constrain(this.scale, 0, 1);
+  }
+}
