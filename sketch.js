588c53dc0b7f31ab81816f10c071c7c75309fab9
ihybrid18542324 <50878460+ihybrid18542324@users.noreply.github.com>
2019-05-28 16:58:03 +0100

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -15,7 +15,7 @@ function preload() {
 }
 
 function setup() {
-  createCanvas(600, 154);
+  createCanvas(600, 600);
 
   // Here we load the pixels in the image and work out
   // what colour each circle will be
